9efd99054a14e9764504e7d5ff6bb35e8eaec215
Mingyu Hsia <myhsia@outlook.com>
2026-06-04 21:47:20 +0800

    Initial the fancydoc package

---
+++ README.md
@@ -0,0 +1,54 @@
+[![CTAN Version](https://img.shields.io/ctan/v/fancydoc)](https://ctan.org/pkg/fancydoc)
+[![GitHub Release](https://img.shields.io/github/v/release/myhsia/fancydoc)](https://github.com/myhsia/fancydoc/releases/latest)
+[![GitHub Last Commit](https://img.shields.io/github/last-commit/myhsia/fancydoc)](https://github.com/myhsia/fancydoc/commits)
+[![Actions Status](https://github.com/myhsia/fancydoc/actions/workflows/main.yaml/badge.svg?branch=main)](https://github.com/myhsia/fancydoc/actions)
+[![GitHub Repo stars](https://img.shields.io/github/stars/myhsia/fancydoc)](https://github.com/myhsia/fancydoc)
+
+The `fancydoc` Package
+=================================
+
+The `fancydoc` package is an extension for documenting the LaTeX source files.
+It is based on the `doc` package and compatible with the `l3doc` classes.
+
+Overview
+--------
+
+To load this template, write the line
+
+    \usepackage[<options>]{fancydoc}
+
+See `fancydoc.pdf` for more. Happy TeXing!
+
+Issues
+------
+
+The issue tracker for `fancydoc` is currently located
+[on GitHub](https://github.com/myhsia/fancydoc/issues).
+
+Build status
+------------
+
+This project uses [GitHub Actions](https://github.com/features/actions)
+as a hosted continuous integration service. For each commit, the build status
+is tested using the current release of TeX Live.
+
+_Current build status:_ ![build status](https://github.com/myhsia/fancydoc/actions/workflows/main.yaml/badge.svg?branch=main)
+
+Copyright and License
+---------------------
+
+Copyright (C) 2026 by Mingyu Xia \<xiamingyu@westlake.edu.cn\>
+
+This work may be distributed and/or modified under the conditions
+of the LaTeX Project Public License (LPPL), either version 1.3c of
+this license or (at your option) any later version.
+The latest version of this license is in
+
+    http://www.latex-project.org/lppl.txt
+
+and version 1.3c or later is part of all distributions of LaTeX
+version 2008 or later.
+
+This work has the LPPL maintenance status `maintained`.
+
+The Current Maintainer of this work is **Mingyu Xia**.
---
+++ build.lua
@@ -0,0 +1,86 @@
+--[==========================================[--
+        L3BUILD FILE FOR WESTLAKETHESIS
+      Once Pushed With This File Modified
+        A New Release Will Be Published
+--]==========================================]--
+
+--[==========================================[--
+               Basic Information
+             Do Check Before Push
+--]==========================================]--
+
+module              = "fancydoc"
+version             = "v0.0.1"
+date                = "2026-06-07"
+maintainer          = "Mingyu Xia"
+uploader            = "Mingyu Xia"
+maintainid          = "myhsia"
+email               = "myhsia@outlook.com"
+repository          = "https://github.com/" .. maintainid .. "/" .. module
+summary             = "Extension for documenting the LaTeX source files"
+description         = "The `fancydoc` package is an extension for documenting the LaTeX source files."
+
+--[==========================================[--
+          Build, Pack, Tag, and Upload
+         Do not Modify Unless Necessary
+--]==========================================]--
+checkengines        = {"pdftex", "xetex", "uptex", "luatex"}
+cleanfiles          = {"*.log", "*.pdf", "*.zip", "*.curlopt"}
+ctanzip             = module
+excludefiles        = {"*~"}
+textfiles           = {"README.md", "LICENSE", "*.lua"}
+typesetexe          = "latexmk -pdf -pdflatex=pdflatex-dev"
+typesetruns         = 1
+uploadconfig  = {
+  note              = "",
+  announcement_file = "announcement.md",
+  pkg               = module,
+  version           = version .. " " .. date,
+  author            = maintainer,
+  uploader          = uploader,
+  email             = email,
+  summary           = summary,
+  description       = description,
+  license           = "lppl1.3c",  
+  ctanPath          = "/macros/latex/contrib/" .. module,
+  home              = "https://github.com/" .. maintainid,
+  bugtracker        = repository .. "/issues",
+  support           = repository .. "/issues",
+  repository        = repository,
+  development       = "https://github.com/" .. maintainid,
+  update            = true
+}
+function update_tag(file, content, tagname, tagdate)
+  tagname = version
+  tagdate = date
+  if string.match(file, module .. ".dtx$") then
+    content = string.gsub(content,
+      "%%<++!driver>\\GetIdInfo $Id: " .. module .. ".dtx " ..
+      "v%d+%.%d+%.%d+ %d+%-%d+%-%d+ (.-)<(.-)>",
+      "%%<+!driver>\\GetIdInfo $Id: "  .. module .. ".dtx " ..
+      tagname .. " " .. tagdate .. " " .. maintainid .. "<" .. email .. ">")
+  end
+  return content
+end
+
+--[== "Hacks" to `l3build` | Do not Modify ==]--
+
+function docinit_hook()
+  cp(ctanreadme, unpackdir, currentdir)
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe
+  if os.getenv("WINDIR") ~= nil or os.getenv("COMSPEC") ~= nil then
+    upretex_aux = "-usepretex=\"" .. typesetcmds .. "\""
+    makeidx_aux = "-e \"$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\""
+    sandbox_aux = "set \"TEXINPUTS=../local;%TEXINPUTS%;\" &&"
+  else
+    upretex_aux = "-usepretex=\'" .. typesetcmds .. "\'"
+    makeidx_aux = "-e \'$makeindex=q/makeindex -s " .. indexstyle .. " %O %S/\'"
+    sandbox_aux = "TEXINPUTS=\"../local:$(kpsewhich -var-value=TEXINPUTS):\""
+  end
+  return run(dir, sandbox_aux .. " " .. cmd         .. " " ..
+                  upretex_aux .. " " .. makeidx_aux .. " " .. file)
+end
